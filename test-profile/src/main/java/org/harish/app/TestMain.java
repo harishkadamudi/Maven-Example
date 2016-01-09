881e385fe37ff898c7c1b82a4b7e47cc55c57b70
harishkadamudi <harishyouk@gmail.com>
2016-01-09 19:35:09 +0530

    Initial Commmit

---
+++ test-profile/src/main/java/org/harish/app/TestMain.java
@@ -0,0 +1,13 @@
+package org.harish.app;
+
+import org.apache.log4j.Logger;
+
+public class TestMain {
+
+	private static final Logger LOGGER = Logger.getLogger(TestMain.class);
+
+	public static void main(String[] args) {
+		LOGGER.debug("Hello Wolrd !!!!");
+	}
+
+}
